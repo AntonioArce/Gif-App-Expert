4eced723a6f66bac798af27c2ed0b6414f60fd6a
AntonioArce <tony.ipn26@gmail.com>
2024-07-03 23:54:09 -0600

    :fire: Ejercicio de Curso React Fernando Herrera

---
+++ src/components/Title.jsx
@@ -0,0 +1,15 @@
+export const Title = () => {
+  return (
+    <>
+        <div className="titulo">
+            <h1>Antonio Arce</h1>
+        </div>
+        <div className="titulo">
+            <h2>Gif App</h2>
+        </div>
+        <div className="titulo">
+            <h2>Curso React: Fernando Herrera</h2>
+        </div>
+    </>
+  )
+}
